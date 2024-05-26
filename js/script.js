5ea5d99895b20a94cac8d93d2d60cab29625c72f
Edom Belayneh <101343364+edombelayneh@users.noreply.github.com>
2024-05-26 17:12:36 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,29 @@
-/*const toggle = document.querySelector('.toggle')
-const navigation = document.querySelector('.navigation')
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
 
-toggle.addEventListener('click', () => {
-    toggle.classList.toggle('active')
-    navigation.classList.toggle('active')
-})
-*/
+    breakpoints:{
+        0:{
+            slidesPerView: 1,
+        },
+        520:{
+            slidesPerView: 2,
+        },
+        950:{
+            slidesPerView: 3,
+        },
+    }
+  });
